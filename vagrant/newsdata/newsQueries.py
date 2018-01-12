1c83a4303331538200c30aee59fba33e3dfe1dc9
Veena0803 <iyerveena08@gmail.com>
2018-01-11 22:30:31 -0500

    Address review comments

--- vagrant/newsdata/newsQueries.py
+++ vagrant/newsdata/newsQueries.py
@@ -1,58 +1,72 @@
+# !/usr/bin/env python2
+
 """Database to connect to newsdata.sql"""
 
-#!/usr/bin/env python2
-
 import psycopg2
-import re
 
 DBNAME = "news"
 
 
-def popular_articles():
-    '''Fetch the most popular articles of all time.'''
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
-    q = """select articles.title, count(*) as num_of_views from articles \
-        join log on articles.slug = \
-        (regexp_split_to_array(path,E'/article/'))[2] where path != '/' \
-        group by (regexp_split_to_array(path, E'/article/'))[2], \
-        articles.title order by num_of_views desc limit 3;"""
-    c.execute(q)
-    posts = c.fetchall()
-    print "\nWhat are the most popular articles of all time?\n"
-    for p in posts:
-        print ' -- '.join(str(item) for item in p) + " views"
+'''Fetch the most popular articles of all time.'''
+art_title = "\nWhat are the most popular articles of all time?\n"
+q = """SELECT articles.title, count(*) AS num_of_views
+FROM articles JOIN log
+ON '/article/' || articles.slug = log.path
+GROUP BY articles.title
+ORDER BY num_of_views DESC
+LIMIT 3;"""
 
-    '''Fetch the most popular article authors of all time.'''
-    r = """select authors.name, count(articles.title)\
-            as views from authors join articles on authors.id = \
-            articles.author join log on articles.slug = \
-            (regexp_split_to_array(path,E'/article/'))[2]\
-            where log.status = '200 OK' group by \
-            authors.name order by views DESC;"""
-    c.execute(r)
-    posts1 = c.fetchall()
-    print "\nWhat are the most popular article authors of all time?\n"
-    for p in posts1:
-        print ' -- '.join(str(item) for item in p) + " views"
+'''Fetch the most popular article authors of all time.'''
+aut_title = "\nWhat are the most popular article authors of all time?\n"
+r = """SELECT authors.name, count(articles.title) AS views
+FROM authors JOIN articles
+ON authors.id = articles.author JOIN log
+ON '/article/' || articles.slug = log.path
+WHERE log.status = '200 OK'
+GROUP BY authors.name
+ORDER BY views DESC;"""
 
-        '''Fetch days that had more than 1% failed requests.'''
+'''Fetch days that had more than 1% failed requests.'''
+fr_title = "\nOn which days did more than 1% of requests lead to errors?\n"
+final_query = """SELECT *
+FROM final_log_view
+WHERE error_percentage >= 1;"""
 
-    drop_command1 = """DROP VIEW IF EXISTS log_view CASCADE;"""
-    drop_command2 = """DROP VIEW IF EXISTS final_log_view CASCADE;"""
-    query1 = """create view log_view as select status,\
-                date(time) as day from log;"""
-    query2 = """create view final_log_view as select day,\
-             100 * (sum(case when status != '200 OK' then 1 else 0 end)\
-             ::float / count(day)::float)\
-             as test from log_view group by day;"""
-    query3 = """select * from final_log_view where test >= 1;"""
-    final_query = drop_command1 + drop_command2 + query1 + query2 + query3
-    c.execute(final_query)
-    posts2 = c.fetchall()
-    print "\nOn which days did more than 1% of requests lead to errors?\n"
-    for p in posts2:
-        print ' -- '.join(str(item) for item in p) + " % error"
-    db.close()
 
-popular_articles()
+def connect():
+    """Connect to the news database"""
+    try:
+        conn = psycopg2.connect(database=DBNAME)
+        cursor = conn.cursor()
+        return conn, cursor
+    except:
+        print("Unable to connect to the database")
+
+
+def get_results(query):
+    "Returns results for all the queries"
+    conn, cursor = connect()
+    cursor.execute(query)
+    return cursor.fetchall()
+    conn.close()
+
+
+def print_results(query_results):
+    print(query_results[1])
+    for title, views in query_results[0]:
+        print('"{}" - {} views'.format(title, views))
+
+
+def print_errors(query_results):
+    print (query_results[1])
+    for time, error_percentage in query_results[0]:
+        print('{} - {} % errors'.format(time, error_percentage))
+
+if __name__ == '__main__':
+    results_popular_articles = get_results(q), art_title
+    results_popular_authors = get_results(r), aut_title
+    results_failed_requests = get_results(final_query), fr_title
+
+    print_results(results_popular_articles)
+    print_results(results_popular_authors)
+    print_errors(results_failed_requests)
